c41268ffb94a3d33e62e59b667288e69726016e6
Vinay7709 <165414618+Vinay7709@users.noreply.github.com>
2024-03-29 23:33:50 +0530

    Create break.java

---
+++ break.java
@@ -0,0 +1,18 @@
+import java.util.scanner;
+public class ch4_break {
+    public static void main(String[] args) {
+      
+    for (int i=0;i<=5;i++)
+    {
+        System.out.println(i);
+        System.out.println("java is popular");
+        if (i==2)
+        {
+            System.out.println("ending the loop");
+break;
+        }
+
+    }
+        System.out.println("loops ends here");    
+    }
+}
